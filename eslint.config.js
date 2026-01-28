b2e4e0eed3283fa2454c41ce9c9eefb079023b24
Ryan Holanda <ryanholanda08@gmail.com>
2026-01-28 20:35:14 -0300

    remove prettier plugin from eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,18 +3,13 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const prettier = require("eslint-config-prettier");
-const prettierPlugin = require("eslint-plugin-prettier");
 
 module.exports = [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   prettier,
   {
-    plugins: {
-      prettier: prettierPlugin,
-    },
     rules: {
-      "prettier/prettier": "error",
       "@typescript-eslint/explicit-function-return-type": "off",
       "@typescript-eslint/no-explicit-any": "warn",
       "@typescript-eslint/no-unused-vars": [
